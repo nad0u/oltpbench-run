ec8034e0a67352eea23cf25b2d10217e518d0063
nad0u <ae.nadia@gmail.com>
2015-05-20 17:00:15 +0200

    added script

---
+++ oltpbench-run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# number of terminals interacting with database
+terminals=40
+# machine name on which the benchmark is run
+machine="large_bs_vm115"
+
+for param in "$@"
+do
+        # store executed commands
+        echo "free -m >> results_s${param}/mem_t${terminals}.txt ; sleep 5 ;" >> results_s${param}/commands.txt
+        # keep track of memory usage
+        free -m >> results_s${param}/mem_t${terminals}.txt ; sleep 5 ;
+
+        for i in {1..2}
+        do
+                # run benchmark
+                echo "./oltpbenchmark -b tpcc -c config/tpcc_s${param}_t${terminals}_config.xml --execute=true -s 1 -o results_s${param}/tpcc_s${param}_t${terminals}_${i}_sh ; sleep 5 ;" >> results_s${param}/commands.txt
+                ./oltpbenchmark -b tpcc -c config/tpcc_s${param}_t${terminals}_config.xml --execute=true -s 1 -o results_s${param}/tpcc_s${param}_t${terminals}_${i}_sh ; sleep 5 ;
+        done
+
+        echo "free -m >> results_s${param}/mem_t${terminals}.txt ; sleep 5 ;" >> results_s${param}/commands.txt
+        free -m >> results_s${param}/mem_t${terminals}.txt ; sleep 5 ;
+        # compress all results files and empty the results folder
+        echo "tar -cvf results_${machine}_s${param}_t${terminals}_time1800.tar results_s${param}/ ; sleep 5 ;" >> results_s${param}/commands.txt
+        tar -cvf results_${machine}_s${param}_t${terminals}_time1800.tar results_s${param}/ ; sleep 5 ;
+        echo "rm -rf results_s${param}/* ; sleep 5 ;" >> results_s${param}/commands.txt
+        rm -rf results_s${param}/* ; sleep 5 ;
+
+done
